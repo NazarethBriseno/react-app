7bbf6967819c094fc8d1e0a6e9d52688d1176cc0
Nazareth-1 <nazarethbriseno@gmail.com>
2023-01-27 12:30:15 -0600

    Learned Components and Passing Data with Props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-
+import Todo from './components/Todo';
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React'/>
+      <Todo  text='Master React'/>
+      <Todo text='Explore full React'/>
+    </div>
+  );
 }
 
 export default App;
